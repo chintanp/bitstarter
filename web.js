bb25558ef14a95d03ac63822d52c4531dae5303d
Chintan Pathak <chintan.pathak@gmail.com>
2013-07-06 09:13:15 +0000

    web.js updated to display index.html

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
- var express = require('express');
+
+
+var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+//Var buf holds the buffer from reading the file index.html
+var buf = fs.readFileSync('./index.html');
+
+ //The variable str is declared to hold the string to be sent to the browser
+var str = buf.toString('ascii');
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
